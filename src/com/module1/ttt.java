9c8eb915cceb3cb89274ba6183c3eaf239762004
owlet_2003@mail.ru <user.gmail.com>
2017-05-12 13:51:28 +0300

    Merge branch 'master' of C:\Users\Red20\iBank with conflicts.

--- src/com/module1/ttt.java
+++ src/com/module1/ttt.java
@@ -1,5 +1,17 @@
-g
-g
-g
-g
-g
+import java.lang.String;
+
+class ttt {
+    private static String ss;
+
+    public static void main(String[] args) {
+        System.out.println("Hi, " + ss);
+    }
+
+    public static String getSs() {
+        return ss;
+    }
+
+    public static void setSs(String ss) {
+        ttt.ss = ss;
+    }
+}
