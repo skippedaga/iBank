9fad6de8f784a0dbe84a0a5d898bc7a228df96f7
owlet_2003@mail.ru <user.gmail.com>
2017-05-12 14:23:48 +0300

    yep (cherry picked from commit b0db848)

---
+++ src/com.company/SuperPuper.java
@@ -0,0 +1,20 @@
+package com.company;
+
+/**
+ * Created by Red20 on 12.05.2017.
+ */
+public class SuperPuper {
+    String string;
+
+    public SuperPuper(String string) {
+        this.string = string;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+}
